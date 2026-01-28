95752a1563b15815a8d4256cf01f7e460f9bd97c
LEDTOMATO <106181127+LEDTOMATO@users.noreply.github.com>
2026-01-29 03:01:46 +0800

    Add Greet and rename chatbot

---
+++ src/main/java/Orange.java
@@ -0,0 +1,12 @@
+public class Orange {
+    public static void main(String[] args) {
+        String greeting =
+                "____________________________________________________________\n" +
+                "Hello! I'm Orange\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(greeting);
+    }
+}

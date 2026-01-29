7116f98c72823305a295bf55b627bf6556e0b5de
LEDTOMATO <106181127+LEDTOMATO@users.noreply.github.com>
2026-01-30 01:22:31 +0800

    Add list storage

--- src/main/java/Orange.java
+++ src/main/java/Orange.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Orange {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
-        String greeting = line +
-                "Hello! I'm Orange\n" +
-                "What can I do for you?\n" + line;
-        System.out.println(greeting);
+        System.out.println(line);
+        System.out.println("Hello! I'm Orange :)\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println(line);
+
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -17,7 +20,22 @@ public class Orange {
                 System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
                 break;
             }
-            System.out.println(line + input +"\n" + line);
+
+            if (input.equals("list")) {
+                System.out.println(line);
+                System.out.println("list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+
+                System.out.println(line);
+                System.out.println("added: " + input);
+                System.out.println(line);
+            }
 
         }
 

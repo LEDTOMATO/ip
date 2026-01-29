3d749a6f61e64cada7eb0430772ff8b984845768
LEDTOMATO <106181127+LEDTOMATO@users.noreply.github.com>
2026-01-30 01:09:46 +0800

    Add Echo and Exit

--- src/main/java/Orange.java
+++ src/main/java/Orange.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Orange {
     public static void main(String[] args) {
-        String greeting =
-                "____________________________________________________________\n" +
+        String line = "____________________________________________________________\n";
+        String greeting = line +
                 "Hello! I'm Orange\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+                "What can I do for you?\n" + line;
         System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
+                break;
+            }
+            System.out.println(line + input +"\n" + line);
+
+        }
+
     }
 }

b6fb7dc4a406cc531f81062e5a21f3d68cf6d6e5
LEDTOMATO <106181127+LEDTOMATO@users.noreply.github.com>
2026-02-06 02:01:39 +0800

    Use LocalDate for deadline and event tasks

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,22 +1,54 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a task that must be completed by a specific date.
  */
 public class Deadline extends Task {
-    protected String by;
+    private LocalDate by;
+
 
     public Deadline(String description, String by) {
         super(description, TaskType.DEADLINE);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
+
 
     @Override
     public String toString() {
-        return super.toString() + " (by: " + by + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        return super.toString() + " (by: " + by.format(formatter) + ")";
     }
+
 
     @Override
     public String toFileString() {
-        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        return "D | " + (isDone ? "1" : "0") + " | "
+                + description + " | " + by.toString();
+
     }
 
 }
+//
+///**
+// * Represents a task that must be completed by a specific date.
+// */
+//public class Deadline extends Task {
+//    protected String by;
+//
+//    public Deadline(String description, String by) {
+//        super(description, TaskType.DEADLINE);
+//        this.by = by;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return super.toString() + " (by: " + by + ")";
+//    }
+//
+//    @Override
+//    public String toFileString() {
+//        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+//    }
+//
+//}

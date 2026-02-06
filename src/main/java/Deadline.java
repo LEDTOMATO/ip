da8f0927dc79aef1d1ba36b5c782372244e5b86d
LEDTOMATO <106181127+LEDTOMATO@users.noreply.github.com>
2026-02-06 10:27:00 +0800

    Update unit test

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -29,26 +29,5 @@ public class Deadline extends Task {
     }
 
 }
-//
-///**
-// * Represents a task that must be completed by a specific date.
-// */
-//public class Deadline extends Task {
-//    protected String by;
-//
-//    public Deadline(String description, String by) {
-//        super(description, TaskType.DEADLINE);
-//        this.by = by;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return super.toString() + " (by: " + by + ")";
-//    }
-//
-//    @Override
-//    public String toFileString() {
-//        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
-//    }
-//
-//}
+
+

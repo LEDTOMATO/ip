7a20402297e790120f57e1977ff755bbb99e1d80
LEDTOMATO <106181127+LEDTOMATO@users.noreply.github.com>
2026-02-06 12:05:46 +0800

    Refactor into OOP structure

--- src/main/java/Todo.java
+++
@@ -1,8 +0,0 @@
-/**
- * Represents a todo task without any date or time constraints.
- */
-public class Todo extends Task {
-    public Todo(String description) {
-        super(description, TaskType.TODO);
-    }
-}
